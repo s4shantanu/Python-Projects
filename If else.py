1069c57f98687777f9d1244c17ae8843208dda6c
Shanatnu Raut <67189790+s4shantanu@users.noreply.github.com>
2020-11-09 00:56:13 +0530

    Add files via upload

---
+++ If else.py
@@ -0,0 +1,34 @@
+# Game Greater and lesser indicator
+print("Type the no.")
+var1 = 6
+var2 = 56
+var3 = int(input())
+if var3>var2:
+    print("Greater")
+elif var3==var2:
+    print("Equal")
+else:
+    print("Lesser")
+
+
+#Checking the program
+list1 = [5, 7, 3]
+print(15 not in list1)
+if 15 not in list1:
+    print("No its not in the list")
+
+
+For Driving Approval
+print("What is your age?")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+
+elif age==18:
+    print("We will think about you")
+
+else:
+    print("You can drive")
+
+
+
