72324c21de6d991641cde6186631db4e757f8c54
Shanatnu Raut <67189790+s4shantanu@users.noreply.github.com>
2020-11-17 11:10:07 +0530

    Add files via upload

---
+++ HealthManagementBot.py
@@ -0,0 +1,109 @@
+# Health Management System
+# Its like ToDo report
+# 3 clients - shanu, munnu and tannu
+
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+
+# Total 6 files
+# write a function that when executed takes as input client name
+# One more function to retrieve exercise or food for any client
+
+
+# bhai ye rha program
+import datetime
+
+
+def gettime():
+    return datetime.datetime.now()
+
+
+def take(k):
+    if k == 1:
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("shanu-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("shanu-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    elif (k == 2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("munnu-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("munnu-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    elif (k == 3):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("tannu-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("tannu-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    else:
+        print("plz enter valid input (1(shanu),2(munnu),3(tannu)")
+
+
+def retrieve(k):
+    if k == 1:
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("shanu-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("shanu-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    elif (k == 2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("munnu-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("munnu-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    elif (k == 3):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("tannu-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("tannu-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    else:
+        print("plz enter valid input (shanu,munnu,tannu)")
+
+
+print("health management system: ")
+a = int(input("Press 1 for log the value and 2 for retrieve "))
+
+if a == 1:
+    b = int(input("Press 1 for shanu 2 for munnu 3 for tannu "))
+    take(b)
+else:
+    b = int(input("Press 1 for shanu 2 for munnu 3 for tannu "))
+    retrieve(b)
+
