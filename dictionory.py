16717c987e7f4c913ac2ace7a3b5b40d9863e027
Shanatnu Raut <67189790+s4shantanu@users.noreply.github.com>
2021-05-06 16:11:48 +0530

    Update dictionory.py

--- dictionory.py
+++ dictionory.py
@@ -4,7 +4,7 @@ Dict = {"ignore":"refuse to take notice of or acknowledge",
         "abandon":"cease to support or look after",
         "exaggerate":"enlarged or altered beyond normal proportions",
         "prejudice":"preconceived opinion that is not based on reason or actual experience",
-        "Shantanu":"In past century he is a king of Hastinapur"}
+        "Shantanu":" In past century he is a king of Hastinapur"}
 print("Enter the Word")
 Data = input()
-print(Data +"means" + Dict[Data])
+print(Data +" means" + Dict[Data])
